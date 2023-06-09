e9aa4b787e5e23db527368d16de0b0a1cc22a054
CODE and DESIGN <code.design.git@gmail.com>
2023-06-09 17:50:44 +0100

    fixing the window size and adjusting text

--- YouDownloader.py
+++ YouDownloader.py
@@ -4,11 +4,11 @@ import customtkinter
 
 # GUI settings
 mode = customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_default_color_theme("blue")
 
 # App settings
 app =  customtkinter.CTk()
-app.geometry("450x240")
+app.geometry("460x250")
 app.title("YouDownloader")
 app.wm_iconbitmap("icon.ico")
 
@@ -19,24 +19,22 @@ def download():
         youtube_object = YouTube(youtube_link)
         video = youtube_object.streams.get_highest_resolution()
         video.download()
+        text.configure(text="Download complete!")
     except:
-        error = customtkinter.CTkLabel(app, text="Something went wrong, \ncheck your internet connection, and the YouTube link", font=("sans-serif", 20)).pack()
+        text.configure(text="Something went wrong, \ncheck your internet connection, and the YouTube link")
 
 # UI (Text, button, entry...)
 title = customtkinter.CTkLabel(app, text="YouDownloader", pady=20,font=("sans-serif", 40))
 url_var = customtkinter.StringVar()
-URL_entry = customtkinter.CTkEntry(app, width=400, height=30, border_width=0, corner_radius=10, textvariable=url_var, font=('sans-serif', 20))
-text = customtkinter.CTkLabel(app, text="Ctrl+V or ⌘+V to paste your youtube link", pady=20,font=("sans-serif", 18))
-submit_button = customtkinter.CTkButton(app, text="Download", command=download)
-finish_text = customtkinter.CTkLabel(app, text="", font=("sans-serif", 20))
-
+URL_entry = customtkinter.CTkEntry(app, width=400, height=30, corner_radius=10, textvariable=url_var, font=('sans-serif', 20))
+text = customtkinter.CTkLabel(app, text="Ctrl+V or ⌘+V to paste your youtube link", pady=20,font=("sans-serif", 19))
+submit_button = customtkinter.CTkButton(app, text="Download", fg_color="#F44336", hover_color="#E53935", command=download)
 
 # Applying the UI to the app
 title.pack()
 URL_entry.pack()
 text.pack()
 submit_button.pack()
-finish_text.pack()
 
 # Running the app
 app.mainloop()
